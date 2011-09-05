c7c7ef235b1705805eda6db0e49374be714a1260
burn2shine <burn2shine@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2011-09-05 05:10:34 +0000

    Move scripts from jzar to rdv google code project.

--- trunk/scripts/json2yaml.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export PYTHONPATH=$HOME/usr/opt/pyyaml/lib/python2.6/site-packages
-python -c '
-import sys, json, yaml
-print yaml.safe_dump(yaml.load(sys.stdin), default_flow_style=False)
-' $@
-
-
--- trunk/scripts/save_db_schema.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pg_dump -s -f ../db/db_schema.sql rdv -U e86020 -W -h arcs-db.vpac.org.au -p 5433
