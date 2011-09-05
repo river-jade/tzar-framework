349377036263bd1ea36e9219f687fee3ac26bd5e
burn2shine <burn2shine@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2011-09-05 05:10:34 +0000

    Move scripts from jzar to rdv google code project.

--- scripts/yaml2json.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export PYTHONPATH=$HOME/usr/opt/pyyaml/lib/python2.6/site-packages
-python -c '
-import sys, json, yaml
-json.dump(yaml.load(sys.stdin), sys.stdout, indent=2)
-print
-' $@
-
