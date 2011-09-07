8fac7a2f884e711e52dd5bd567583cf462273cb6
burn2shine <burn2shine@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2011-09-07 06:13:53 +0000

    Implement dynamic repetitions.

---
+++ trunk/java/au/edu/rmit/tzar/api/LinearStepGenerator.java
@@ -0,0 +1,36 @@
+package au.edu.rmit.tzar.api;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Generates a sequence of decimal numbers beginning at start, with a step-size of stepSize,
+ * and "count" elements.
+ */
+public class LinearStepGenerator extends RepetitionGenerator<BigDecimal> {
+  @SerializedName("start")
+  private final BigDecimal start;
+  @SerializedName("count")
+  private final int count;
+  @SerializedName("step_size")
+  private final BigDecimal stepSize;
+
+  public LinearStepGenerator(String key, BigDecimal start, int count, BigDecimal stepSize) {
+    super(key);
+    this.start = start;
+    this.count = count;
+    this.stepSize = stepSize;
+  }
+
+  @Override
+  public List<BigDecimal> generate() {
+    List<BigDecimal> results = new ArrayList<BigDecimal>();
+    for (int i = 0; i < count; ++i) {
+      results.add(start.add(stepSize.multiply(BigDecimal.valueOf(i))));
+    }
+    return results;
+  }
+}
--- trunk/javatest/au/edu/rmit/tzar/api/JsonParserTest.java
+++ trunk/javatest/au/edu/rmit/tzar/api/JsonParserTest.java
@@ -2,12 +2,15 @@ package au.edu.rmit.tzar.api;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import junit.framework.TestCase;
 
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -51,5 +54,24 @@ public class JsonParserTest extends TestCase {
     jsonParser.projectSpecToJson(projectSpec, tempFile);
     ProjectSpec projectSpecCopy = jsonParser.projectSpecFromJson(tempFile);
     assertEquals(projectSpecCopy, projectSpec);
+  }
+
+  /**
+   * Tests that parsing a json string containing 2 static repetitions and two generators
+   * (each generating 10 values), gives a set of parameter sets (with each set unique) of size
+   * 200 (2 * 10 * 10).
+   *
+   * @throws RdvException
+   */
+  public void testRepetitionDeserialisationAndGeneration() throws RdvException {
+    String json = "{ repetitions : [ { variables : { A : 1 } }, { variables : " +
+        "{ A : 2 } } ], generators : [ { key : B, generator_type : linear_step, start : 0, step_size : 0.1, " +
+        "count : 10 }, { key : C, generator_type : linear_step, start : 1, step_size : 1, " +
+        "count : 10 } ] }";
+    Repetitions repetitions = jsonParser.repetitionsFromJson(json);
+    List<Parameters> paramsList = repetitions.getParamsList();
+    HashSet<Parameters> set = Sets.newHashSet(paramsList);
+    assertEquals(200, paramsList.size());
+    assertEquals(set.size(), paramsList.size()); // assert that all items in the list are unique
   }
 }
