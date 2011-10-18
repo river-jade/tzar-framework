44fbe8e61c2829c52813b6c68aa2535844102fef
burn2shine <burn2shine@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2011-10-18 04:04:35 +0000

    Adds RandomNullRunner and moves NullRunner to .runners

--- java/au/edu/rmit/tzar/runners/RandomNullRunner.java
+++ java/au/edu/rmit/tzar/runners/RandomNullRunner.java
@@ -1,4 +1,4 @@
-package au.edu.rmit.tzar;
+package au.edu.rmit.tzar.runners;
 
 import au.edu.rmit.tzar.api.Parameters;
 import au.edu.rmit.tzar.api.RdvException;
@@ -7,12 +7,13 @@ import au.edu.rmit.tzar.api.Runner;
 import java.io.File;
 
 /**
- * Runner implementation that does nothing.
+ * Runner implementation that does nothing and returns success with 50% probability.
+ * (Note that this is not truly random!)
  */
-public class NullRunner implements Runner {
+public class RandomNullRunner implements Runner {
   @Override
   public boolean runModel(File model, File outputPath, String runId, String flags, Parameters parameters)
       throws RdvException {
-    return true;
+    return System.currentTimeMillis() % 2 == 0;
   }
 }
