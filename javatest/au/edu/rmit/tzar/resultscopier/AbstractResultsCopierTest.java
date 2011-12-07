ce3c08359321979a1630848c27cc6a4b1e7b658a
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2011-12-07 02:55:27 +0000

    Modify SCPResultsCopierTest to use mocks, rather than relying on a local ssh server.

--- javatest/au/edu/rmit/tzar/resultscopier/AbstractResultsCopierTest.java
+++ javatest/au/edu/rmit/tzar/resultscopier/AbstractResultsCopierTest.java
@@ -14,36 +14,23 @@ import java.io.File;
  */
 public abstract class AbstractResultsCopierTest extends TestCase {
   ResultsCopier copier;
-  File baseDestDir;
+  File baseDestPath;
+  File sourcePath;
+  File sourcePath2;
+  protected File localOutputPath;
+  protected Run run;
 
   @Override
   public void setUp() throws Exception {
     super.setUp();
-    baseDestDir = Files.createTempDir();
-  }
-
-  /**
-   * Creates a zero-length file in /tmp and attempts to copy it to another temp directory,
-   * and checks that they are equal.
-   *
-   * @throws Exception if thrown, test has failed
-   */
-  public void testCopyResults() throws Exception {
     File tempSourceDir = Files.createTempDir();
-
-    Run run = new Run(1234, "a run", "4321", "", Parameters.EMPTY_PARAMETERS, "scheduled", "");
+    run = new Run(1234, "a run", "4321", "", Parameters.EMPTY_PARAMETERS, "scheduled", "");
     ExecutableRun executableRun = ExecutableRun.createExecutableRun(run, tempSourceDir, null, new NullRunner());
-    File localOutputPath = executableRun.getOutputPath();
+    localOutputPath = executableRun.getOutputPath();
     localOutputPath.mkdir();
-    File sourcePath = File.createTempFile("file", null, localOutputPath);
-    File sourcePath2 = File.createTempFile("file", null, localOutputPath);
-
-    copier.copyResults(run, localOutputPath);
-
-    File destDir = new File(baseDestDir, localOutputPath.getName());
-    File destPath1 = new File(destDir, sourcePath.getName());
-    File destPath2 = new File(destDir, sourcePath2.getName());
-    assertTrue(Files.equal(sourcePath, destPath1));
-    assertTrue(Files.equal(sourcePath2, destPath2));
+    baseDestPath = Files.createTempDir();
+    sourcePath = File.createTempFile("file", null, localOutputPath);
+    sourcePath2 = File.createTempFile("file", null, localOutputPath);
   }
+
 }
--- javatest/au/edu/rmit/tzar/resultscopier/FileResultsCopierTest.java
+++ javatest/au/edu/rmit/tzar/resultscopier/FileResultsCopierTest.java
@@ -1,5 +1,9 @@
 package au.edu.rmit.tzar.resultscopier;
 
+import com.google.common.io.Files;
+
+import java.io.File;
+
 /**
  * Tests that the local filesystem copier copies data as expected.
  */
@@ -7,6 +11,22 @@ public class FileResultsCopierTest extends AbstractResultsCopierTest {
   @Override
   public void setUp() throws Exception {
     super.setUp();
-    copier = new FileResultsCopier(baseDestDir);
+    copier = new FileResultsCopier(baseDestPath);
+  }
+
+  /**
+   * Creates a zero-length file in /tmp and attempts to copy it to another temp directory,
+   * and checks that they are equal.
+   *
+   * @throws Exception if thrown, test has failed
+   */
+  public void testCopyResults() throws Exception {
+    copier.copyResults(run, localOutputPath);
+
+    File destDir = new File(baseDestPath, localOutputPath.getName());
+    File destPath1 = new File(destDir, sourcePath.getName());
+    File destPath2 = new File(destDir, sourcePath2.getName());
+    assertTrue(Files.equal(sourcePath, destPath1));
+    assertTrue(Files.equal(sourcePath2, destPath2));
   }
 }
