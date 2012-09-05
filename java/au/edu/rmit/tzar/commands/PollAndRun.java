a99d79a83ffdfb1c457750864a468b48d385161f
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2012-09-05 04:35:53 +0000

    Fix getHostName method to work on Nectar machines.

--- java/au/edu/rmit/tzar/commands/PollAndRun.java
+++ java/au/edu/rmit/tzar/commands/PollAndRun.java
@@ -13,7 +13,6 @@ import au.edu.rmit.tzar.resultscopier.ResultsCopier;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.UnknownHostException;
 import java.util.Date;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -139,7 +138,7 @@ class PollAndRun implements Command {
     run.setStartTime(new Date());
     run.setEndTime(null);
     run.setState("in_progress");
-    run.setHostname(getHostname());
+    run.setHostname(Utils.getHostname());
     runDao.persistRun(run);
 
     executorService.submit(new DbExecutableRun(executableRun, resultsCopier, runDao, new Callback() {
@@ -148,17 +147,6 @@ class PollAndRun implements Command {
         runningTasks.release(); // release the semaphore now that the task is done
       }
     }));
-  }
-
-  private static String getHostname() {
-    String hostname;
-    try {
-      hostname = Utils.getHostname();
-    } catch (UnknownHostException e) {
-      LOG.log(Level.WARNING, "Unable to determine hostname. Setting to 'UNKNOWN'");
-      hostname = "UNKNOWN";
-    }
-    return hostname;
   }
 
   private static interface Callback {
