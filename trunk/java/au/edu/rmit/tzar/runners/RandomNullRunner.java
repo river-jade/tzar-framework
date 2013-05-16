44d95a7960d19e45fe88b26533c74e89919b23a5
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2013-05-16 06:37:30 +0000

    Make RandomNullRunner actually random

--- trunk/java/au/edu/rmit/tzar/runners/RandomNullRunner.java
+++ trunk/java/au/edu/rmit/tzar/runners/RandomNullRunner.java
@@ -1,21 +1,23 @@
 package au.edu.rmit.tzar.runners;
 
 import au.edu.rmit.tzar.api.Parameters;
-import au.edu.rmit.tzar.api.TzarException;
 import au.edu.rmit.tzar.api.Runner;
+import au.edu.rmit.tzar.api.TzarException;
 
 import java.io.File;
+import java.util.Random;
 import java.util.logging.Logger;
 
 /**
  * Runner implementation that does nothing and returns success with 50% probability.
- * (Note that this is not truly random!)
  */
 public class RandomNullRunner implements Runner {
+  private final Random random = new Random();
+
   @Override
   public boolean runModel(File model, File outputPath, String runId, String flagsString, Parameters parameters,
       Logger logger) throws TzarException {
-    boolean success = System.currentTimeMillis() % 2 == 0;
+    boolean success = random.nextBoolean();
     logger.info("Run " + (success ? "succeeded" : "failed"));
     return success;
   }
