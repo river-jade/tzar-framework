fc8c14e7072cc4fbc28aee0e873ca4dbde51b988
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2013-10-27 01:07:08 +0000

    Add javadoc target to build.xml and commit generated docs to source where they can be browsed on google code.

---
+++ java/au/edu/rmit/tzar/repository/CodeSource.java
@@ -0,0 +1,11 @@
+package au.edu.rmit.tzar.repository;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michaell
+ * Date: 24/10/13
+ * Time: 11:02 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CodeSource {
+}
