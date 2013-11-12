b3629b0a067c3ce159b0bcd1cd7d9a59d8504c71
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2013-11-12 01:26:35 +0000

    Fixes bug in PollAndRun where we were retrying immediately on error with no backoff. Also, polling rate was being read from the spinner rate (once / sec), rather than the flag. - Refactor PollAndRun to use java.util.Timer for simplicity.

--- trunk/java/au/edu/rmit/tzar/commands/PollAndRun.java
+++ trunk/java/au/edu/rmit/tzar/commands/PollAndRun.java
@@ -1,21 +1,23 @@
 package au.edu.rmit.tzar.commands;
 
+import au.edu.rmit.tzar.Constants;
 import au.edu.rmit.tzar.ExecutableRun;
 import au.edu.rmit.tzar.RunnerFactory;
 import au.edu.rmit.tzar.Utils;
-import au.edu.rmit.tzar.api.TzarException;
 import au.edu.rmit.tzar.api.Run;
+import au.edu.rmit.tzar.api.TzarException;
 import au.edu.rmit.tzar.db.RunDao;
 import au.edu.rmit.tzar.resultscopier.ResultsCopier;
 
 import java.io.File;
 import java.util.Date;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,13 +31,10 @@ import static au.edu.rmit.tzar.commands.SharedFlags.RUNNER_FLAGS;
  */
 class PollAndRun implements Command {
   private static final Logger LOG = Logger.getLogger(PollAndRun.class.getName());
-  private static final String STOP_FILE_NAME = "stop.now";
   public static final Object[] FLAGS = new Object[]{CommandFlags.POLL_AND_RUN_FLAGS, DB_FLAGS, RUNNER_FLAGS};
 
   private final RunDao runDao;
-  private final AtomicBoolean exitWhenDone = new AtomicBoolean(false);
-  private final CountDownLatch finished = new CountDownLatch(1);
-  private final int sleepTimeMillis;
+  private final int pollRateMs;
   private final String runset;
   private final ResultsCopier resultsCopier;
   private final ExecutorService executorService;
@@ -49,7 +48,7 @@ class PollAndRun implements Command {
   /**
    * Constructor.
    * @param runDao for accessing the database
-   * @param sleepTimeMillis milliseconds to wait between polls
+   * @param pollRateMs milliseconds to wait between polls
    * @param resultsCopier to copy the results from this node to permanent storage
    * @param runset name of the runset to poll, or null to poll all runsets
    * @param clusterName name of the cluster which this node is running on
@@ -58,14 +57,14 @@ class PollAndRun implements Command {
    * @param runnerFactory to create runners
    * @param repositoryUriPrefixes list of allowed repository uri prefixes
    */
-  public PollAndRun(RunDao runDao, int sleepTimeMillis, ResultsCopier resultsCopier, String runset,
+  public PollAndRun(RunDao runDao, int pollRateMs, ResultsCopier resultsCopier, String runset,
       String clusterName, int concurrentTaskCount, File baseOutputPath, File baseModelPath,
       RunnerFactory runnerFactory, List<String> repositoryUriPrefixes) {
     this.baseOutputPath = baseOutputPath;
     this.baseModelPath = baseModelPath;
     this.runnerFactory = runnerFactory;
     this.runDao = runDao;
-    this.sleepTimeMillis = sleepTimeMillis;
+    this.pollRateMs = pollRateMs;
     this.runset = runset;
     this.clusterName = clusterName;
     this.resultsCopier = resultsCopier;
@@ -90,7 +89,7 @@ class PollAndRun implements Command {
     this.runnerFactory = runnerFactory;
     this.runDao = runDao;
     this.baseModelPath = baseModelPath;
-    this.sleepTimeMillis = POLL_AND_RUN_FLAGS.getSleepTimeMillis();
+    this.pollRateMs = POLL_AND_RUN_FLAGS.getPollRateMs();
     this.runset = POLL_AND_RUN_FLAGS.getRunset();
     this.clusterName = POLL_AND_RUN_FLAGS.getClusterName();
     this.resultsCopier = resultsCopier;
@@ -99,72 +98,22 @@ class PollAndRun implements Command {
     repositoryUriPrefixes = POLL_AND_RUN_FLAGS.getRepositoryUriPrefixes();
   }
 
-
   /**
    * Polls the database for new runs, and loops until exitAfterCurrent run is called (and current run finishes).
    * Sleeps 10 seconds between each poll if there are no runs to execute.
    *
    * @throws InterruptedException if the thread is interrupted
    */
+  @Override
   public boolean execute() throws InterruptedException {
-    // start the file copier thread
-    int spinnerDelay = 1000; // rotate spinner on stdout every 1 sec.
-    try {
-      int spinCounter = 0;
-      int pollCounter = 0;
-      char[] symbol = new char[]{'-', '\\', '|', '/'};
-      String backspace = "";
-      while (!exitWhenDone.get()) {
-        if (checkForStopFile()) {
-          return true;
-        }
-        LOG.finer("Polling for next run.");
-        Run run = null;
-        try {
-          while (true) {
-            runningTasks.acquire(); // this line will block if there are already max tasks running
-            run = runDao.getNextRun(runset, clusterName);
-            if (run == null) {
-              runningTasks.release();
-              break;
-            }
-            executeRun(run);
-          }
-          if (spinCounter * spinnerDelay >= pollCounter * sleepTimeMillis) {
-            pollCounter++;
-          }
-        } catch (TzarException e) {
-          LOG.log(Level.SEVERE, "Error occurred executing run.", e);
-          System.out.println("\n");
-          runningTasks.release();
-        }
+    final Spinner spinner = new Spinner().start();
+    final CountDownLatch stop = new CountDownLatch(1);
+    final Timer timer = new Timer();
+    final TimerTask task = new PollTask(stop, spinner, timer);
+    timer.schedule(task, 0 /* start polling straightaway */);
 
-        if (run == null) { // no run found. wait for 10 secs.
-          Thread.sleep(spinnerDelay);
-          System.out.print(backspace + symbol[spinCounter++ % 4]);
-          System.out.flush();
-          backspace = "\b";
-        } else {
-          backspace = "";
-        }
-      }
-    } finally {
-      finished.countDown();
-    }
+    stop.await(); // wait until the poll task signals that it's done
     return true;
-  }
-
-  private static boolean checkForStopFile() {
-    File stopFile = new File(System.getProperty("user.dir"), STOP_FILE_NAME);
-    if (stopFile.exists()) {
-      if (!stopFile.delete()) {
-        LOG.warning("Could not delete stop file: " + stopFile);
-      }
-      LOG.info("Found stop file. Exiting.");
-      return true;
-    } else {
-      return false;
-    }
   }
 
   private void executeRun(final Run run) throws TzarException, InterruptedException {
@@ -183,11 +132,32 @@ class PollAndRun implements Command {
 
     executorService.submit(new DbExecutableRun(executableRun, resultsCopier, runDao, repositoryUriPrefixes,
         new Callback() {
-      @Override
-      public void complete() {
-        runningTasks.release(); // release the semaphore now that the task is done
+          @Override
+          public void complete() {
+            runningTasks.release(); // release the semaphore now that the task is done
+          }
+        }));
+  }
+
+  /**
+   * Poll the database and execute runs until there are no more runs in the scheduled state.
+   * @throws TzarException
+   * @throws InterruptedException
+   */
+  private void pollUntilDone() throws TzarException, InterruptedException {
+    runningTasks.acquire(); // this line will block if there are already max tasks running
+    try {
+      while (true) {
+        LOG.finer("Polling for next run.");
+        Run run = runDao.getNextRun(runset, clusterName);
+        if (run == null) {
+          return;
+        }
+        executeRun(run);
       }
-    }));
+    } finally {
+      runningTasks.release();
+    }
   }
 
   private static interface Callback {
@@ -274,6 +244,97 @@ class PollAndRun implements Command {
       LOG.warning("The sourceUrl for this run (" + sourceUri + ") did not begin with any of the allowed prefixes (" +
           repositoryUriPrefixes + "). Failing run as a security measure.");
       return false;
+    }
+  }
+
+  private class PollTask extends TimerTask {
+    private final CountDownLatch stop;
+    private final Spinner spinner;
+    private final Timer timer;
+    private int pollInterval;
+
+    public PollTask(CountDownLatch stop, Spinner spinner, Timer timer) {
+      this(stop, spinner, timer, pollRateMs);
+    }
+
+    private PollTask(CountDownLatch stop, Spinner spinner, Timer timer, int pollInterval) {
+      this.stop = stop;
+      this.spinner = spinner;
+      this.timer = timer;
+      this.pollInterval = pollInterval;
+    }
+
+    @Override
+    public void run() {
+      if (checkForStopFile()) {
+        stop.countDown();
+        return;
+      }
+      // stop the spinner while we poll.
+      spinner.pause();
+      // keep polling until there are no more runs
+      int nextPollInterval;
+      try {
+        pollUntilDone();
+        nextPollInterval = pollRateMs; // success! reset poll interval to standard
+      } catch (Exception e) {
+        System.out.println("\n"); // so that the exception is on a new line
+        LOG.log(Level.SEVERE, "Error occurred executing run.", e);
+        // exponentially back off the poll interval up to a maximum.
+        nextPollInterval = Math.min(pollInterval * 2, Constants.MAX_POLL_INTERVAL_MS);
+        LOG.log(Level.INFO, "Polling again in {0} seconds.", nextPollInterval / 1000);
+      } finally {
+        spinner.resume();
+      }
+      TimerTask task = new PollTask(stop, spinner, timer, nextPollInterval);
+      timer.schedule(task, nextPollInterval);
+    }
+
+    private boolean checkForStopFile() {
+      File stopFile = new File(System.getProperty("user.dir"), Constants.STOP_FILE_NAME);
+      if (stopFile.exists()) {
+        if (!stopFile.delete()) {
+          LOG.warning("Could not delete stop file: " + stopFile);
+        }
+        LOG.info("Found stop file. Exiting.");
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  private static class Spinner extends TimerTask {
+    private static final String BACKSPACE = "\b";
+    private static final char[] SYMBOL = new char[]{'-', '\\', '|', '/'};
+    private int spinCounter = 0;
+    private boolean first = true;
+    private volatile boolean renderSpinner = true;
+    private Timer timer = new Timer();
+
+    public Spinner start() {
+      renderSpinner = true;
+      int interval = Constants.SPINNER_ROTATION_INTERVAL_MS;
+      timer.scheduleAtFixedRate(this, interval, interval);
+      return this;
+    }
+
+    public Spinner pause() {
+      renderSpinner = false;
+      return this;
+    }
+
+    public Spinner resume() {
+      renderSpinner = true;
+      return this;
+    }
+
+    @Override
+    public void run() {
+      if (renderSpinner) {
+        System.out.print((first ? "" : BACKSPACE) + SYMBOL[spinCounter++ % 4]);
+        System.out.flush();
+      }
     }
   }
 }
