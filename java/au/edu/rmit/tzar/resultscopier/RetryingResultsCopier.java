7dc9151f9d0c68d74105d8ff5683775ed18c8575
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2013-11-18 04:34:38 +0000

    Delete duplicated retry code from ScpResultsCopier, and refactor RetryingResultsCopier to use Utility method.

--- java/au/edu/rmit/tzar/resultscopier/RetryingResultsCopier.java
+++ java/au/edu/rmit/tzar/resultscopier/RetryingResultsCopier.java
@@ -1,11 +1,10 @@
 package au.edu.rmit.tzar.resultscopier;
 
+import au.edu.rmit.tzar.Utils;
 import au.edu.rmit.tzar.api.Run;
 import au.edu.rmit.tzar.api.TzarException;
-import com.google.common.base.Optional;
 
 import java.io.File;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -23,29 +22,13 @@ public class RetryingResultsCopier implements ResultsCopier {
   }
 
   @Override
-  public void copyResults(Run run, File sourcePath, boolean success) throws TzarException {
-    Optional<TzarException> e = Optional.absent();
-    for (int i = 0; i < retryCount; i++) {
-      if (e.isPresent()) {
-        LOG.log(Level.WARNING, "Error copying results for run: " + run + ". Error: " + e.get().getMessage() +
-            " Retry: " + i);
+  public void copyResults(final Run run, final File sourcePath, final boolean success) throws TzarException {
+    Utils.Retryable.retryWithBackoff(retryCount, 1000, new Utils.Retryable() {
+      @Override
+      public void exec() throws TzarException {
+        delegate.copyResults(run, sourcePath, success);
       }
-
-      e = copyResultsInternal(run, sourcePath, success);
-      if (!e.isPresent()) { // no error, success!
-        return;
-      }
-    }
-    throw e.get();
-  }
-
-  private Optional<TzarException> copyResultsInternal(Run run, File sourcePath, boolean success) {
-    try {
-      delegate.copyResults(run, sourcePath, success);
-      return Optional.absent();
-    } catch (TzarException e) {
-      return Optional.of(e);
-    }
+    });
   }
 }
 
--- java/au/edu/rmit/tzar/resultscopier/ScpResultsCopier.java
+++ java/au/edu/rmit/tzar/resultscopier/ScpResultsCopier.java
@@ -35,47 +35,41 @@ public class ScpResultsCopier implements ResultsCopier {
 
   @Override
   public void copyResults(final Run run, final File sourcePath, boolean success) throws TzarException {
-    // Retry copying 8 times, doubling the wait in between each attempt, up to 2 minutes wait.
-    Utils.Retryable.retryWithBackoff(8, 1000, new Utils.Retryable() {
-      @Override
-      public void exec() throws TzarException {
-        String hostname = sshClientFactory.getHostname();
-        LOG.info("Copying results from: " + sourcePath + " to " + hostname + ":" + baseDestPath);
-        try {
-          boolean thrown = true;
-          SSHClient sshClient = null;
-          try {
-            // we create a new ssh connection for each copy attempt, as we were having issues with
-            // connections dropping and not reconnecting. this is a bit less efficient, but more robust.
-            sshClient = sshClientFactory.createSSHClient();
-            SCPFileTransfer scpFileTransfer = sshClient.newSCPFileTransfer();
+    String hostname = sshClientFactory.getHostname();
+    LOG.info("Copying results from: " + sourcePath + " to " + hostname + ":" + baseDestPath);
+    try {
+      boolean thrown = true;
+      SSHClient sshClient = null;
+      try {
+        // we create a new ssh connection for each copy attempt, as we were having issues with
+        // connections dropping and not reconnecting. this is a bit less efficient, but more robust.
+        sshClient = sshClientFactory.createSSHClient();
+        SCPFileTransfer scpFileTransfer = sshClient.newSCPFileTransfer();
 
-            // slight hack here because the SCPFileTransfer class upload method doesn't accept a File object for
-            // the destination path (ie only accepts a String), and if this is run on a windows machine where the
-            // separator character is '\', a single directory gets created on the destination with name a\b\c,
-            // instead of a tree a/b/c.
-            // This hack may break if the ssh server is a windows machine.
-            scpFileTransfer.upload(new FileSystemFile(sourcePath),
-                baseDestPath.getPath().replace(File.separatorChar, '/'));
-            run.setRemoteOutputPath(baseDestPath);
-            run.setOutputHost(hostname);
+        // slight hack here because the SCPFileTransfer class upload method doesn't accept a File object for
+        // the destination path (ie only accepts a String), and if this is run on a windows machine where the
+        // separator character is '\', a single directory gets created on the destination with name a\b\c,
+        // instead of a tree a/b/c.
+        // This hack may break if the ssh server is a windows machine.
+        scpFileTransfer.upload(new FileSystemFile(sourcePath),
+            baseDestPath.getPath().replace(File.separatorChar, '/'));
+        run.setRemoteOutputPath(baseDestPath);
+        run.setOutputHost(hostname);
 
-            Utils.deleteRecursively(sourcePath.getCanonicalFile());
-            LOG.log(Level.INFO, "Copied results for run: {0,number,#} from: {1} to {2}:{3}",
-                new Object[]{run.getRunId(), sourcePath, hostname, baseDestPath});
-            thrown = false;
-          } finally {
-            if (sshClient != null) {
-              // if an exception was thrown, we make sure we don't
-              // throw another one while closing the connection (as this would mean that the first one
-              // would be lost).
-              Closeables.close(sshClient, thrown);
-            }
-          }
-        } catch (IOException e) {
-          throw new TzarException(e);
+        Utils.deleteRecursively(sourcePath.getCanonicalFile());
+        LOG.log(Level.INFO, "Copied results for run: {0,number,#} from: {1} to {2}:{3}",
+            new Object[]{run.getRunId(), sourcePath, hostname, baseDestPath});
+        thrown = false;
+      } finally {
+        if (sshClient != null) {
+          // if an exception was thrown, we make sure we don't
+          // throw another one while closing the connection (as this would mean that the first one
+          // would be lost).
+          Closeables.close(sshClient, thrown);
         }
       }
-    });
+    } catch (IOException e) {
+      throw new TzarException(e);
+    }
   }
 }
