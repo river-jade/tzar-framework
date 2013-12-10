97ec56f149672d24b730b44dd78c7210284ea164
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2013-12-10 03:27:26 +0000

    Use revision 343 of R library. Required to work with current version of R runner. Also fix bug in example R project code.

--- example-projects/example-R/model.R
+++ example-projects/example-R/model.R
@@ -23,12 +23,8 @@ cat('Value for test variable 4 is:', parameters$'test.variable.4', '\n')
 cat('The working dir is', getwd(), '\n')
 cat('test.output.filename=', parameters$'test.output.filename', '\n')
 
-cat('\n\n##The current working dir is', getwd(), '\n\n' )
-
 test.text <- rep(1:10)
 
 write.table(test.text, parameters$'test.output.filename' )
 write.table(x, parameters$'test.output.filename2' )
 str(parameters$'test.output.filename')
-
-str(variables)
