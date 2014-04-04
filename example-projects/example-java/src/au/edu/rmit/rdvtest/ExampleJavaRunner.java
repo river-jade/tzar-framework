00b8e2ee5dd5626b0908fbc731bf1f1ed90b551e
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2014-04-04 09:38:02 +0000

    Improve map reduce: - make the reduce phase more sane, now each call to reduce takes a set of files with the same name from across runs.  - change Concatenator to support heading rows  - add a FileCopier Mapper to support renaming  - support loading mappers and reducers from arbitrary packages.

--- example-projects/example-java/src/au/edu/rmit/rdvtest/ExampleJavaRunner.java
+++ example-projects/example-java/src/au/edu/rmit/rdvtest/ExampleJavaRunner.java
@@ -5,6 +5,8 @@ import au.edu.rmit.tzar.api.Runner;
 import au.edu.rmit.tzar.api.TzarException;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 /**
@@ -18,6 +20,17 @@ public class ExampleJavaRunner implements Runner {
     logger.info("Entering example java runner.");
     logger.info("Here are the parameters: " + parameters.toString());
     logger.info("Exiting example java runner.");
+    File file = new File(outputPath, "single_result.csv");
+    logger.info(file.toString());
+    FileWriter writer;
+    try {
+      writer = new FileWriter(file);
+      writer.write("value 1,value 2,value 3\n");
+      writer.write(String.format("%s1,%s2,%s3\n", runId, runId, runId));
+      writer.close();
+    } catch (IOException e) {
+      throw new TzarException(e);
+    }
     return true;
   }
 }
--- java/au/edu/rmit/tzar/runners/mapreduce/Mapper.java
+++ java/au/edu/rmit/tzar/runners/mapreduce/Mapper.java
@@ -1,16 +1,25 @@
 package au.edu.rmit.tzar.runners.mapreduce;
 
+import au.edu.rmit.tzar.api.TzarException;
+
 import java.io.File;
 import java.util.Map;
 import java.util.Set;
 
 /**
  * Interface for the mapper function. Mapper maps from a run output directory to
- * a set of file paths. It may do any required transformation as required, though
- * it's recommended that the Mapper does not overwrite existing output files.
+ * a set of file paths. It may do any required transformation as required, including creating new files,
+ * though it's recommended that the Mapper does not overwrite existing output files.
  */
 public interface Mapper {
-  Set<File> map(File runDirectory);
+  /**
+   * Given a fully qualified run output directory for a single run, this method does
+   * any required work with the files in that directory, including potentially writing more files to
+   * the output directory, or to a temporary location, and returns a Set of files for the reduce phase.
+   * @param runDirectory
+   * @return
+   */
+  Set<File> map(File runDirectory) throws TzarException;
 
   void setFlags(Map<String, String> flags);
 
