a57306eef0a2d951fba761e53866b7ec21c57a25
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2014-05-06 04:30:07 +0000

    More fixes to create_db_node.sh

--- trunk/db/create_db_node.sh
+++ trunk/db/create_db_node.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 set -e # -e means exit on any error
 set -u # -u means treat unset variables as errors
-set -o pipefail # handles corner case where pipe destination command fails
+# set -o pipefail # handles corner case where pipe destination command fails
 
 password=${1:?Need to pass the password for the tzar db user as the first parameter}
 
 # install postgresql
-yes | sudo apt-get update && yes | sudo apt-get install postgresql
+sudo apt-get update && yes | sudo apt-get install postgresql
 
 # download schema
 wget https://tzar-framework.googlecode.com/svn/trunk/db/db_schema.sql
 
-# become postgres user
-sudo su postgres
+sudo su postgres <<EOF
 
 # create the tzar user (without a password)
 createuser tzar -S -d -R
 
 # set the password for the tzar user
 
-psql -c "alter user postgres with password '$password';"
+psql -c "alter user tzar with password '$password';"
 
 # create tzar database
 createdb tzar -O tzar
@@ -28,11 +27,11 @@ createdb tzar -O tzar
 # password login, and connections from internet, and restart db server
 sed '/^local.*all.*all.*peer/s/peer/trust/g' -i /etc/postgresql/9.1/main/pg_hba.conf
 
-cat >> /etc/postgresql/9.1/main/pg_hba.conf << EOF
+cat >> /etc/postgresql/9.1/main/pg_hba.conf << EOF_INNER
 
 #Allow network connections from anywhere using password auth
 host    all             all             0.0.0.0/0               md5
-EOF
+EOF_INNER
 
 sed '/^#listen_addresses/a\
   listen_addresses = '"'"*"'" /etc/postgresql/9.1/main/postgresql.conf -i
@@ -41,3 +40,4 @@ sed '/^#listen_addresses/a\
 #Execute db creation script
 psql -f ~ubuntu/db_schema.sql tzar -U tzar
 
+EOF
