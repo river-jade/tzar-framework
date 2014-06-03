cb52e876ad1b4d7fa8a5dfab67e8c251dbfa14d5
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2014-06-03 04:52:41 +0000

    Add example of http_library to example-java.

--- trunk/example-projects/example-java/src/au/edu/rmit/rdvtest/ExampleJavaRunner.java
+++ trunk/example-projects/example-java/src/au/edu/rmit/rdvtest/ExampleJavaRunner.java
@@ -4,9 +4,12 @@ import au.edu.rmit.tzar.api.Parameters;
 import au.edu.rmit.tzar.api.Runner;
 import au.edu.rmit.tzar.api.TzarException;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.String;
 import java.util.logging.Logger;
 
 /**
@@ -16,7 +19,7 @@ import java.util.logging.Logger;
 public class ExampleJavaRunner implements Runner {
 
   public boolean runModel(File model, File outputPath, String runId, String flagsString, Parameters parameters,
-        Logger logger) throws TzarException {
+      Logger logger) throws TzarException {
     logger.info("Entering example java runner.");
     logger.info("Here are the parameters: " + parameters.toString());
     logger.info("Exiting example java runner.");
@@ -28,6 +31,15 @@ public class ExampleJavaRunner implements Runner {
       writer.write("value 1,value 2,value 3\n");
       writer.write(String.format("%s1,%s2,%s3\n", runId, runId, runId));
       writer.close();
+    } catch (IOException e) {
+      throw new TzarException(e);
+    }
+
+    BufferedReader reader;
+    try {
+      String pathname = parameters.asMap().get("version.library.name").toString();
+      reader = new BufferedReader(new FileReader(new File(pathname)));
+      logger.info("Latest tzar version is: " + reader.readLine());
     } catch (IOException e) {
       throw new TzarException(e);
     }
