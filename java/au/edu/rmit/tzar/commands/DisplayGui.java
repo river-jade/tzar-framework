082d03c5a51c9eb678917d849899f95128d77dfd
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2014-06-30 07:18:28 +0000

    Move app name property settings out of TzarGui due to mac/java weirdness.

--- java/au/edu/rmit/tzar/commands/DisplayGui.java
+++ java/au/edu/rmit/tzar/commands/DisplayGui.java
@@ -9,6 +9,8 @@ import au.edu.rmit.tzar.gui.TzarGui;
 public class DisplayGui implements Command {
   @Override
   public boolean execute() throws TzarException {
+    // below is so we get correct app name in mac menu. Doesn't work if we do it in the TzarGui constructor!!
+    System.setProperty("com.apple.mrj.application.apple.menu.about.name", TzarGui.APP_NAME);
     new TzarGui().display();
     return true;
   }
