393b5127c42f3a44f163e206cd18551e7cd32cf5
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2014-06-30 07:04:01 +0000

    Factor path to version.properties into a constant

--- java/au/edu/rmit/tzar/api/Constants.java
+++ java/au/edu/rmit/tzar/api/Constants.java
@@ -45,4 +45,5 @@ public class Constants {
 
   public static final String HEAD_REVISION = "head";
   public static final String METADATA_DIRECTORY_NAME = "metadata";
+  public static final String VERSION_PROPERTIES = "/version.properties";
 }
