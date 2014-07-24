17407f70c07c6572bf45c0420dc78e4f4bafbc58
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2014-07-24 01:42:32 +0000

    Add flags to aggregate runs to skip copying runs that already exist locally, and to start copying from a given run id.

--- trunk/java/au/edu/rmit/tzar/commands/AggregateResults.java
+++ trunk/java/au/edu/rmit/tzar/commands/AggregateResults.java
@@ -12,16 +12,21 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.google.common.io.Files;
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.xfer.scp.SCPFileTransfer;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static au.edu.rmit.tzar.commands.SharedFlags.DB_FLAGS;
 import static au.edu.rmit.tzar.commands.SharedFlags.LOAD_RUNS_FLAGS;
@@ -53,6 +58,8 @@ class AggregateResults implements Command {
    */
   private final LoadingCache<String, SSHClient> connections;
   private final Utils.RegexFilter regexFilter;
+  private final boolean skipExistingRuns;
+  private final int startRunId;
 
   /**
    * Constructor.
@@ -77,8 +84,12 @@ class AggregateResults implements Command {
     this.filterHostname = filterHostname;
     this.runset = runset;
     this.destPath = flags.getOutputPath();
+    this.skipExistingRuns = flags.isSkipExistingRuns();
+    this.startRunId = flags.getStartRunId();
     this.runDao = runDao;
     this.hostname = hostname;
+
+    // We use a cache builder so that we can memoize and reuse ssh connections, as they're expensive to create.
     connections = CacheBuilder.newBuilder().build(new CacheLoader<String, SSHClient>() {
       @Override
       public SSHClient load(String sourceHost) {
@@ -102,16 +113,30 @@ class AggregateResults implements Command {
   @Override
   public boolean execute() throws TzarException {
     List<Run> runs = runDao.getRuns(states, filterHostname, runset, runIds);
+    Set<Integer> previouslyCopiedRuns = getPreviouslyCopiedRuns();
+
     try {
       for (Run run : runs) {
+        // check if we already have files with that run_id, and if so, skip this run.
+        if (skipExistingRuns && previouslyCopiedRuns.contains(run.getRunId())) {
+          LOG.info(String.format("Results found locally for run: %d. Skipping copying this run.", run.getRunId()));
+          continue;
+        }
+
+        if (run.getRunId() < startRunId) {
+          LOG.info(String.format("Run %d less that start run id. Skipping copying this run.", run.getRunId()));
+          continue;
+        }
+
         LOG.info("Copying results for run: " + run);
         String sourceHost = run.getOutputHost();
-        File runOutputPath = run.getRemoteOutputPath();
+        File remoteOutputPath = run.getRemoteOutputPath();
         if (hostname.equals(sourceHost)) {
-          LOG.fine("Results are on localhost. Using copy to copy results.");
-          Utils.copyDirectory(runOutputPath, destPath, new RunIdRenamer(run.getRunId()), regexFilter);
+          LOG.fine("Results are on localhost. Using file copy to copy results.");
+          Utils.copyDirectory(remoteOutputPath, destPath, new RunIdRenamer(run.getRunId()), regexFilter);
         } else {
           LOG.fine("Results are on machine: " + sourceHost + ". Using ssh to copy results.");
+
           SCPFileTransfer scpFileTransfer;
           try {
             scpFileTransfer = connections.get(sourceHost).newSCPFileTransfer();
@@ -119,9 +144,9 @@ class AggregateResults implements Command {
             throw new TzarException(e.getCause());
           }
           File tempPath = Files.createTempDir();
-          scpFileTransfer.download(runOutputPath.getPath(), tempPath.getPath());
-          Utils.copyDirectory(new File(tempPath, runOutputPath.getName()),
-              destPath, new RunIdRenamer(run.getRunId()), regexFilter);
+          scpFileTransfer.download(remoteOutputPath.getPath(), tempPath.getPath());
+          File localPath = new File(tempPath, remoteOutputPath.getName());
+          Utils.copyDirectory(localPath, destPath, new RunIdRenamer(run.getRunId()), regexFilter);
         }
       }
     } catch (IOException e) {
@@ -136,6 +161,26 @@ class AggregateResults implements Command {
       }
     }
     return true;
+  }
+
+  /**
+   * Returns Set containing runs that have already been copied (by the heuristic that there exist files in
+   * the output directory starting with "<runid>_".
+   * @return
+   */
+  private Set<Integer> getPreviouslyCopiedRuns() {
+    File[] files = destPath.listFiles();
+    if (files == null) { return Sets.newHashSet(); }
+
+    Set<Integer> copiedRuns = new HashSet<Integer>(files.length);
+    Pattern pattern = Pattern.compile("(\\d+)_.*");
+    for (File file : files) {
+      Matcher matcher = pattern.matcher(file.getName());
+      if (matcher.matches()) {
+        copiedRuns.add(Integer.parseInt(matcher.group(1)));
+      }
+    }
+    return copiedRuns;
   }
 
   private class RunIdRenamer implements Utils.RenamingStrategy {
