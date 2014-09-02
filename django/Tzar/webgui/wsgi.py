e3516b90b6bb09a61e861b9076e1a701b3b3db64
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2014-09-02 02:03:53 +0000

    Rebuild wsgi file.

--- django/Tzar/webgui/wsgi.py
+++ django/Tzar/webgui/wsgi.py
@@ -7,7 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "temp.settings")
 
-application = Cling(get_wsgi_application())
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
