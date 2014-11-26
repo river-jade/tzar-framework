7c1609cdb2c861bef27a7a6892369b7adc420b70
burn2shine@gmail.com <burn2shine@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2014-11-26 02:16:08 +0000

    Some minor cleanup of example projects, and makes Jython example write to output files similarly to python example.

--- trunk/example-projects/example-jython/model.py
+++ trunk/example-projects/example-jython/model.py
@@ -10,9 +10,19 @@ class Model(basemodel.BaseModel):
 
         # gets the variables, with (java) decimal values converted to python decimals
         # this is useful if you want to use arithmetic operations within python.
-        variables = self.get_decimal_params(runparams)
+        params = self.get_decimal_params(runparams)
 
-        self.logger.fine("\nRandom.seed is %s" % variables['random.seed'])
-        self.logger.fine("\nTest variable 4 is the string: %s" % variables['test.variable.4'])
-        self.logger.fine("\nPAR.testing.output.filename is: %s" % variables['PAR.testing.output.filename'])
-        self.logger.fine("\nPAR.testing.output.filename2 is: %s" % variables['PAR.testing.output.filename2'])
+        self.logger.fine("\nRandom.seed is %s" % params['random.seed'])
+        self.logger.fine("\nTest variable 4 is the string: %s" % params['test.variable.4'])
+        self.logger.fine("\noutput.filename is: %s" % params['output.filename'])
+        self.logger.fine("\noutput.filename2 is: %s" % params['output.filename2'])
+
+        # write some params to output files
+        f1 = open(params["output.filename"], 'w')
+        f1.write(str(params["test.variable.1"]) + "\n")
+        f1.close()
+
+        f2 = open(params["output.filename2"], 'w')
+        f2.write(str(params["test.variable.2"]) + "\n")
+        f2.close()
+
