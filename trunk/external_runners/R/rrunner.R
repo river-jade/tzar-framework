75f94f5cb728c3ade581207f90beda8f0cfa5aee
langfob@gmail.com <langfob@gmail.com@978bd849-9e5e-2e34-b5f9-def03b34cd01>
2015-02-22 23:27:39 +0000

    BTL: Modified the RRunner so that it prints more meaningful output when R throws an exception.  There was an error in the way that it printed using cat() that prevented the printing of the error message.  I fixed that and made some changes that will also force the printing of the traceback() results when there is a fatal error.

--- trunk/external_runners/R/rrunner.R
+++ trunk/external_runners/R/rrunner.R
@@ -6,10 +6,34 @@
 # --paramfile, and --rscript, which are the json file containing the
 # parameters, and the rscript to be executed, respectively.
 
+#-------------------------------------------------------------------------------
+#
+#  History: 
+#
+#   - Created by River Satya.
+#
+#   - Modified by Ascelin Gordon
+#
+#   - 2015 02 13 - Modified by Bill Langford
+#       - Fixed the way the tryCatch handles errors so that it will 
+#         properly print error messages when there is a crash, plus it 
+#         will print the traceback of the stack.
+#       - Before this change, an error would produce the same odd error 
+#         message no matter what the error was.  The message would say 
+#         something about cat not being able to print a list.  This was 
+#         due to a cat statement in the error branch of the tryCatch 
+#         trying to print the error condition object, which appears to be 
+#         a list.  The "print" statement can do this but "cat" can't do it.
+#         The fix here completely changes the way all that is handled 
+#         because it was useful to get the traceback, etc.
+#
+#-------------------------------------------------------------------------------
 
-# Define a function to check if a package is installed and perfrom a
-# personal install if it is not (this function uses a personal install to avoid
-# issue with not having permissions for a system install).
+    #--------------------------------------------------------------------------
+    # Define a function to check if a package is installed and perfrom a
+    # personal install if it is not (this function uses a personal install to 
+    # avoid issue with not having permissions for a system install).
+    #--------------------------------------------------------------------------
 
 
 install.if.required <- function(pkg) {
@@ -68,15 +92,52 @@ parameters <- tzar$parameters
 # for debugging: prints out the parameters
 # str(tzar)
 
+    #--------------------------------------------------------------------------
+    #  Creating a global flag to indicate whether an error has been trapped 
+    #  in the RRunner.  Tried to make the name unique enough not to be 
+    #  accidentally interfering with a variable name anywhere else in the 
+    #  code.  
+    #
+    #  Have not found a way to get the traceback and correct handling of 
+    #  error from inside the tryCatch without using a global variable. 
+    #
+    #  Also setting the default error handler to get the traceback since 
+    #  it doesn't seem possible to get the traceback from inside the tryCatch 
+    #  (see comments in tryCatch below for more detail).
+    #--------------------------------------------------------------------------
+
+failed_in____RRunner = FALSE
+options(error = 
+            function() 
+                { 
+                message ("\n\n-----  FATAL ERROR IN R CODE  -----: \n"); 
+                traceback(2); 
+                    #  VERY IMPORTANT THAT THIS IS A GLOBAL ASSIGNMENT
+                failed_in____RRunner <<- TRUE    
+                }
+        )
+
    #--------------------------------------------------------------
    # Source the project's R script
    #--------------------------------------------------------------
 
-tryCatch( 
-    
-    source(args$rscript),
-    warning = function(warning)  cat( " *** WARNING in rrunner.R:", warning ), 
-    error = function(error) cat( " *** ERROR in rrunner.R:", error ), 
+failed_in____RRunner = tryCatch( 
+    {
+    failed_in____RRunner = FALSE
+    source (args$rscript)
+    },
+        #  BTL - 2015 02 13
+        #  Removed both the warning and the error branches of this tryCatch.
+        #  The warning part was unnecessary since R will spit those out 
+        #  automatically.
+        #  The error part had to be handled by the global "options(error = " 
+        #  statement to get the traceback included in the error output.  
+        #  For some reason, traceback is not accessible inside a tryCatch: 
+        #  the R help for traceback says:
+        #      "Errors which are caught via try or tryCatch do not generate 
+        #       a traceback, so what is printed is the call sequence for the 
+        #       last uncaught error, and not necessarily for the last error."
+
     finally = {
 
           #--------------------------------------------------------------
@@ -91,5 +152,13 @@ tryCatch(
         si <- paste( outputpath, '/metadata/R_sessionInfo.txt', sep='')
         writeLines(capture.output(date(), cat("\n"), sessionInfo()), con=si)
     }
-
 )
+
+if (failed_in____RRunner) 
+    {
+        #  Setting the return status to 10 to match the R help for the 
+        #  q() function's recommendation about return status.
+    
+    q (save = "no", status = 10, runLast = FALSE)
+    }
+
