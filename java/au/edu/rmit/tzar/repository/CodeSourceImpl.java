19cb27afbeebc3947140739311254cc9aaaca38b
Michael Lancaster <michael@michaellancaster.org>
2015-07-20 11:36:41 +1000

    Add eclipse git client.

--- java/au/edu/rmit/tzar/repository/CodeSourceImpl.java
+++ java/au/edu/rmit/tzar/repository/CodeSourceImpl.java
@@ -145,11 +145,12 @@ public class CodeSourceImpl implements CodeSource {
     GIT {
       @Override
       public CodeRepository createRepository(CloseableHttpClient httpClient, URI sourceUri, boolean downloadOnce) {
-        throw new UnsupportedOperationException("Sorry, Git repository support is not yet implemented.");
+        return new GitRepository(sourceUri);
       }
 
       @Override
       public boolean isValidRevision(String revision) {
+        // FIXME
         return true;
       }
     },
